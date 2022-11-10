f8ba7f1b3e2aa64c1b9e9f68fd66e57de625dfb3
dominikTanko <dominik.tanko23@gmail.com>
2022-11-10 14:26:05 +0100

    Initialize logic behind questions

---
+++ js/questions.js
@@ -0,0 +1,45 @@
+const container = document.querySelector(".container");
+let points = 0;
+let currentLevel = 1;
+let currentQuestion = 0;
+
+async function main(currentLevel, currentQuestion) {
+    let data = await fetch("/quiz/questionsLvl" + currentLevel + ".json");
+    let questions = await data.json();
+
+    createPage(questions[currentQuestion])
+}
+
+function createPage(question) {
+    let answers = "";
+    question.answers.forEach(({answer, correct}) => 
+        (answers += `<button class="answerButton" type="button" id="userReponse" onClick="verifyAnswer(${correct})">${answer}</button>`)
+    );
+    let output = `
+            <h2>Points : ${points}</h2>
+            <img class="questionImg" src=${question.img} />
+            <p>${question.question}</p>
+            ${answers}
+            `
+    container.innerHTML = output;
+}
+
+function verifyAnswer(isCorrect) {
+    if (isCorrect) {
+        points++;
+        if (currentQuestion == 0) {
+            currentQuestion++;
+        } else if (currentLevel < 3) {
+            currentLevel++;
+            currentQuestion = 0;
+        } else {
+            container.innerHTML = `
+                <h1>CONGRATULATIONS!</h1>
+                <h2>Points : ${points}</h2>
+                `;
+            return;
+        }
+        main(currentLevel, currentQuestion)
+    }
+}
+
